489d34571d1483d1f23ed134ea5f3d4c0fe81ff4
BuildTools <unconfigured@null.spigotmc.org>
2020-11-09 11:30:42 +0000

    Updated gaussian to be more readable

--- 2_Statistical_modelling_I:Introduction/practicals/loglikelihoods/gaussian.ll.R
+++ 2_Statistical_modelling_I:Introduction/practicals/loglikelihoods/gaussian.ll.R
@@ -1,9 +1,11 @@
 gaussian.ll <- function( x, params = list( mean = 1, sigma2 = 1 )) {
-    # Handle negative sigmas
+    mean = params$mean
+    sigma2 = params$sigma2
+
     if( min( params$sigma2 ) < 0 ) {
         return( -Inf )
     }
-    with( params, -0.5 * ( log(sigma2) + log( 2*pi ) + (x-mean)^2/sigma2 ) )
-    # or use built-in function:
-    #dnorm( x, mean = 1, sd = sqrt( params$variance ), log = T )
+
+    result = -0.5 * log( 2*pi*sigma2 ) - (x-mean)^2/(2*sigma2)
+    return( result )
 }
