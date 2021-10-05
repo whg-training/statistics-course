0a7073d8542bc4a13f1439fbf93db9968660eb42
Gavin Band <gavin.band@well.ox.ac.uk>
2021-10-05 16:22:19 +0100

    Added solutions/part3/

--- Genes/practicals/Genes across species/Getting_sequence_lengths.md
+++ Genes/practicals/Genes across species/Getting_sequence_lengths.md
@@ -5,32 +5,18 @@ each genome is. Happily, if you look at the [GFF files on
 Ensembl](http://ftp.ensembl.org/pub/current_gff3/) the sequences from the relevant reference are
 listed in the metadata.  For example, for humans they look like this:
 
-   ##gff-version 3
-   ##sequence-region   1 1 248956422
-   ##sequence-region   10 1 133797422
-   ##sequence-region   11 1 135086622
-   ##sequence-region   12 1 133275309
-   ##sequence-region   13 1 114364328
-   ##sequence-region   14 1 107043718
-   ##sequence-region   15 1 101991189
-   ##sequence-region   16 1 90338345
-   ##sequence-region   17 1 83257441
-   ##sequence-region   18 1 80373285
-   ##sequence-region   19 1 58617616
-   ##sequence-region   2 1 242193529
-   ##sequence-region   20 1 64444167
-   ##sequence-region   21 1 46709983
-   ##sequence-region   22 1 50818468
-   ##sequence-region   3 1 198295559
-   ##sequence-region   4 1 190214555
-   ##sequence-region   5 1 181538259
-   ##sequence-region   6 1 170805979
-   ##sequence-region   7 1 159345973
-   ##sequence-region   8 1 145138636
-   ##sequence-region   9 1 138394717
-   ##sequence-region   MT 1 16569
-   ##sequence-region   X 1 156040895
-   ##sequence-region   Y 2752083 56887902
+    ##gff-version 3
+    ##sequence-region   1 1 248956422
+    ##sequence-region   10 1 133797422
+    ##sequence-region   11 1 135086622
+    ##sequence-region   12 1 133275309
+    ...
+    ##sequence-region   7 1 159345973
+    ##sequence-region   8 1 145138636
+    ##sequence-region   9 1 138394717
+    ##sequence-region   MT 1 16569
+    ##sequence-region   X 1 156040895
+    ##sequence-region   Y 2752083 56887902
 
 The three pieces of data on each row are: the sequence name (first column), the sequence starting
 position, and the ending position. (The Y chromosome starts at a base different than 1, because it
---
+++ Genes/practicals/Genes across species/solutions/part3/gff.py
@@ -0,0 +1,74 @@
+# gff.py
+# This file implements the function parse_gff3_to_dataframe()
+# and a number of helper functions.
+
+def parse_gff3_to_dataframe( file ):
+    """Read GFF3-formatted data in the specified file (or file-like object)
+    Return a pandas dataframe with ID, Parent, seqid, source, type, start, end, score, strand, phase, and attributes columns.
+    The ID and Parent are extracted from the attributes columns, and the dataframe is indexed by ID"""
+    result = read_gff3_using_pandas( file )
+    extract_attributes_to_columns( result, ['ID', 'Parent', 'Name', 'biotype'] )
+    return result
+
+# functions starting with underscores are private to the file
+def read_gff3_using_pandas( file ):
+    """Helper function to read the given GFF3 file into a dataframe, without any postprocessing."""
+    import pandas
+    result = pandas.read_table(
+        file,
+        comment = '#',
+        names = [ 'seqid', 'source', 'type', 'start', 'end', 'score', 'strand', 'phase', 'attributes' ],
+        na_values = ".",
+        dtype = {
+            'seqid': str,
+            'source': str,
+            'type': str,
+            'start': int,
+            'end': int,
+            'score': float,
+            'strand': str,
+            'phase': str,
+            'attributes': str
+        }
+    )
+    return result
+
+def extract_attributes_to_columns( data, attributes_to_extract = [ 'ID', 'Parent' ] ):
+    # The original function called parse_attributes twice - and was slow.
+    # The second version called it once to unpack them all, but used masses of memory.
+    # This third version uses regular expression to parse the attributes, get the value
+    # and remove the extracted ones from the string directly - without duplicating the memory.
+    import re
+    def getAttribute( entry, regexp ):
+        m = re.search( regexp, entry )
+        return None if m is None else m.group(1)
+    def removeAttribute( entry, regexp ):
+        return re.sub( regexp, "", entry )
+
+    for i in range( 0, len(attributes_to_extract)):
+        attribute = attributes_to_extract[i]
+        regexp = re.compile( "%s=([^;]+);?" % attribute )
+        data.insert( i, attribute, data['attributes'].apply( lambda entry: getAttribute( entry, regexp ) ))
+        # Delete the field from the current attributes
+        # (I could not get .transform() work here for some reason)
+        data['attributes'] = data['attributes'].apply( lambda entry: removeAttribute( entry, regexp ))
+
+def parse_sequences_from_gff_metadata( file ):
+    """GFF3 files from the Ensembl ftp site list sequences and their lengths in the file metadata.
+    This function parses this information and returns it as a pandas dataframe.
+    It's use may be specific to the Ensembl files."""
+    import pandas
+    result = []
+    for line in file:
+        if line.startswith( '##sequence-region' ):
+            parts = line.strip().split( " " )
+            nameStartEnd = parts[-3:] # last 3 elements
+            result.append({
+                "seqid": nameStartEnd[0],
+                "start": int( nameStartEnd[1] ),
+                "end": int( nameStartEnd[2] )
+            })
+        elif not line[0] == '#':
+            # quit when we meet the first non-metadata line
+            break
+    return pandas.DataFrame( result )
